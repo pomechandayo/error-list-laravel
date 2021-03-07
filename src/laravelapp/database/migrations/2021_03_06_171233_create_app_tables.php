eb4a0a4cc187ba7c269dd8d653f4365b401af645
ポメ <pomechan.0505@icloud.com>
2021-03-07 14:08:31 +0900

    マイグレーション実行

---
+++ src/laravelapp/database/migrations/2021_03_06_171233_create_app_tables.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {   
+        if(Schema::hasTable('users')){
+            return;
+        }
+        if(Schema::hasTable('articles')){
+            return;
+        }
+        if(Schema::hasTable('tags')){
+            return;
+        }
+        if(Schema::hasTable('likes')){
+            return;
+        }
+
+        Schema::create('likes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+
+        Schema::create('tags',function
+        (Blueprint $table){
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+        Schema::create('articles',function
+        (Blueprint $table){
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+        Schema::create('users',function
+        (Blueprint $table){
+            $table->bigIncrements('id');
+            $table->string('name',255);
+            $table->string('email',255);
+            $table->string('password',15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+        Schema::dropIfExists('tags');
+        Schema::dropIfExists('articles');
+        Schema::dropIfExists('users');
+    }
+}
