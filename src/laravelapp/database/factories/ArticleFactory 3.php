c0193053d02bc765995e5422fa8264602192e1d3
ポメ <pomechan.0505@icloud.com>
2021-04-08 16:55:18 +0900

    ブランチ切り替え練習

---
+++ src/laravelapp/database/factories/ArticleFactory 3.php
@@ -0,0 +1,28 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+use App\Article;
+use App\Tag;
+
+$factory->define(Article::class, 
+    function(Faker $faker) {
+        return [
+        'title' => $faker->text(10),
+        'body' => $faker->text(50),
+        'user_id' =>$faker->numberBetween(1,3),
+        'status' => $faker->randomElement([1,1,1,1,0]),
+    ];
+});
+
+$factory->define(Tag::class,
+    function(Faker $faker){
+        $tags = 
+        ['PHP','Go','Python','Mysql','laravel', 'Ruby','Docker','javascript','AWS','Linux','Java','C',
+        ];
+        $tag = array_rand($tags,1);
+        return['name' => $tags[$tag],
+    ];
+});
