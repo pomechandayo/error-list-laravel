6e447687d6e3422847dd9d577ac6c42dd62774a5
RyushiMasa <pomechan.0505@icloud.com>
2021-08-29 16:53:46 +0900

    テスト用データベースをsqliteに設定。circleci設定

--- src/laravelapp/config/database.php
+++ src/laravelapp/config/database.php
@@ -35,7 +35,7 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
+        'testing_sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
             'database' => env('DB_DATABASE', database_path('laravel_db.sqlite')),
